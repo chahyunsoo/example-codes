e91fbefd5e5f724e93db0d1a966e712bc3a3f294
chahyunsoo <chahs1012@naver.com>
2023-07-22 01:57:42 +0900

    Collection Example

---
+++ java-example/src/main/java/org/javaexample/Collection/CollectionExample.java
@@ -0,0 +1,20 @@
+package org.javaexample.Collection;
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionExample {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("tom", "park", "kim");
+        list = new ArrayList<>(list);
+//        System.out.println(list.toString());
+        System.out.println(list);
+
+        Collection<Integer> collection = Arrays.asList(1, 2, 3);
+        ArrayList<Integer> arrayList = new ArrayList<>(collection);
+        System.out.println(arrayList);
+
+    }
+}
--- java-example/src/main/java/org/javaexample/ObjectFinalize.java
+++ java-example/src/main/java/org/javaexample/ObjectFinalize.java
@@ -1,27 +1,27 @@
-package org.javaexample;
-
-class Person{
-    String country;
-
-    public Person(String country) {
-        this.country = country;
-    }
-    protected void finalize() throws Throwable {
-        super.finalize();
-        System.out.println("Destroyed: " + country);
-    }
-}
-public class ObjectFinalize {
-    public static void main(String[] args) {
-        Person p1 = new Person("Korea");
-        Person p2 = new Person("Japan");
-        p1 = null;
-        p2 = null;
-
-        System.gc();
-        System.runFinalization();
-
-        System.out.println("Enough");
-
-    }
-}
+//package org.javaexample;
+//
+//class Person{
+//    String country;
+//
+//    public Person(String country) {
+//        this.country = country;
+//    }
+//    protected void finalize() throws Throwable {
+//        super.finalize();
+//        System.out.println("Destroyed: " + country);
+//    }
+//}
+//public class ObjectFinalize {
+//    public static void main(String[] args) {
+//        Person p1 = new Person("Korea");
+//        Person p2 = new Person("Japan");
+//        p1 = null;
+//        p2 = null;
+//
+//        System.gc();
+//        System.runFinalization();
+//
+//        System.out.println("Enough");
+//
+//    }
+//}
